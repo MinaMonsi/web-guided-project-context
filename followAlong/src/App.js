f10bf355df276776c2f5cd4baf7268179cae30ee
Josh Jacobson <joshjacobson14@gmail.com>
2021-04-28 17:41:53 -0700

    lecture work-in-progress

--- followAlong/src/App.js
+++ followAlong/src/App.js
@@ -15,9 +15,11 @@ export default function App() {
   // Contexts must be capitalized becuase they're used as components, and JSX interprets uppercase as components
 
   // Step 2: Provide the Context (by wrapping Context.Provider around the components that will need to consume the Context)
+
+  console.log(FamilyContext);
   return (
     <div className="App">
-      <FamilyContext.Provider>
+      <FamilyContext.Provider value={activeFamily}>
         <section className="header">
           <h1>Family Trees</h1>
           {families.map(family => (
