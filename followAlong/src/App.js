e7be199e17c37b7999e4df00543f923e1af626ea
Josh Jacobson <joshjacobson14@gmail.com>
2021-04-28 17:38:39 -0700

    lecture work-in-progress

--- followAlong/src/App.js
+++ followAlong/src/App.js
@@ -11,6 +11,8 @@ export default function App() {
 
   export const FamilyContext = createContext(); // initialize later, when providing the context to a component tree
 
+  // Contexts must be capitalized becuase they're used as components, and JSX interprets uppercase as components
+
   return (
     <div className="App">
       <section className="header">
