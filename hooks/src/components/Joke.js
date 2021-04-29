e1509efbba21a8ea92f488b556eedb68672c71ad
Josh Jacobson <joshjacobson14@gmail.com>
2021-04-28 17:00:39 -0700

    lecture work-in-progress

--- hooks/src/components/Joke.js
+++ hooks/src/components/Joke.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { connect, useDispatch, useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchJoke } from "../actions/jokeActions";
 
 // If loading, display a loading message
