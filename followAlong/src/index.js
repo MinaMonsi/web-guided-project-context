799a4f532ed68ccd68ea5539fe832bda6af3ce06
Josh Jacobson <joshjacobson14@gmail.com>
2021-04-28 17:08:16 -0700

    lecture work-in-progress

--- followAlong/src/index.js
+++ followAlong/src/index.js
@@ -5,3 +5,7 @@ import App from "./App";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
+
+
+// Two design philosophies (in general, not specific to React or any language/framework)
+// 1. Convention over Configuration (e.g., iPhone, Mac, Ruby on Rails, Redux, Mandalorian)
