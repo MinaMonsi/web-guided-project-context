8498836d63f29e7490753231e38c78a6f96d9d6e
Josh Jacobson <joshjacobson14@gmail.com>
2021-04-28 18:34:03 -0700

    lecture work complete! Yeehaw

--- followAlong/src/index.js
+++ followAlong/src/index.js
@@ -9,4 +9,4 @@ ReactDOM.render(<App />, rootElement);
 
 // Two design philosophies (in general, not specific to React or any language/framework)
 // 1. Convention over Configuration, opinionated design (e.g., iPhone, Mac, Ruby on Rails, Redux, Mandalorian)
-// 2. Konfiguration is King: More open, configurable design (e.g., Android, Linux, JavaScript, Context API)
+// 2. Konfiguration is King: More open, configurable design (e.g., Android, Linux, JavaScript, Context API, YOLO swag choose your own adventure)
