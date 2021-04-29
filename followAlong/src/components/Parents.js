f62581c18b2457c65b88d6a770711297c612f637
Josh Jacobson <joshjacobson14@gmail.com>
2021-04-28 17:52:58 -0700

    lecture work-in-progress

--- followAlong/src/components/Parents.js
+++ followAlong/src/components/Parents.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import { FamilyContext } from '../contexts';
 
+// For class components, use render props pattern with Context.Consumer (rather than the nicer useReducer hook -- hooks are only for function components!)
+
 class Parents extends React.Component {
   render() {
     return (
       <section className="parents">
         <FamilyContext.Consumer>
-        {this.props.family.parents.map(p => (
-          <div className="person" key={p.name}>
-            <img width="150" src={p.img} alt={p.name} />
-            <strong>{p.name}</strong>
-          </div>
-        ))}
+          {family => (
+          <>
+            {
+              this.props.family.parents.map(p => (
+                <div className="person" key={p.name}>
+                  <img width="150" src={p.img} alt={p.name} />
+                  <strong>{p.name}</strong>
+                </div>
+              ))
+            }
+          )}
         </FamilyContext.Consumer>
       </section>
     );
