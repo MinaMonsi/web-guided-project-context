c725aac615f2fd412e8a6f1006d169b0cecd80e9
Josh Jacobson <joshjacobson14@gmail.com>
2021-04-28 17:40:16 -0700

    lecture work-in-progress

--- followAlong/src/App.js
+++ followAlong/src/App.js
@@ -9,10 +9,12 @@ export default function App() {
   const [families] = useState(data);
   const [activeFamily, setActiveFamily] = useState(families[0]);
 
+  // Step 1: create a Context
   export const FamilyContext = createContext(); // initialize later, when providing the context to a component tree
 
   // Contexts must be capitalized becuase they're used as components, and JSX interprets uppercase as components
 
+  // Step 2: Provide the Context (by wrapping Context.Provider around the components that will need to consume the Context)
   return (
     <div className="App">
       <FamilyContext.Provider>
