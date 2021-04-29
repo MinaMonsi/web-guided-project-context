2cd5fad24ca04f156f92328efe26c1c40c44bc28
Josh Jacobson <joshjacobson14@gmail.com>
2021-04-28 17:36:17 -0700

    lecture work-in-progress

--- followAlong/src/App.js
+++ followAlong/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { createContext, useState } from "react";
 
 import { data } from "./data";
 
@@ -8,6 +8,8 @@ import "./styles.scss";
 export default function App() {
   const [families] = useState(data);
   const [activeFamily, setActiveFamily] = useState(families[0]);
+
+  export const FamilyContext = createContext();
 
   return (
     <div className="App">
